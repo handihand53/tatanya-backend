42a1e75d50ea88c187b5c2cb18ec7fa001ff2784
Handi Hermawan <handihand53@gmail.com>
2023-06-03 21:13:22 +0700

    first init tatanya-backend

---
+++ tatanya-backend-app/src/test/java/com/tatanya/backend/app/TestApplication.java
@@ -0,0 +1,7 @@
+package com.tatanya.backend.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.tatanya.backend")
+public class TestApplication {
+}
